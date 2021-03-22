290af91cddcd52d1ab9aab5a2c5ab597a3b4e1c3
engrjunaidali <80405587+engrjunaidali@users.noreply.github.com>
2021-03-22 18:31:45 +0500

    Add files via upload

---
+++ 3. Variables.php
@@ -0,0 +1,32 @@
+<?php
+
+//Your php code goes here...
+
+//Declaring Variables
+
+// In PHP, a variable starts with the $ sign, followed by the name of the variable
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+// Rules for PHP variables:
+
+//     A variable starts with the $ sign, followed by the name of the variable
+//     A variable name must start with a letter or the underscore character
+//     A variable name cannot start with a number
+//     A variable name can only contain alpha-numeric characters and underscores    (A-z, 0-9, and _ )
+//     Variable names are case-sensitive ($age and $AGE are two different variables)
+
+// Output Variables
+
+$txt = "<br>Pakistan";
+echo "I love $txt!";
+
+echo "<br>";
+
+$x = 5;
+$y = 4;
+echo $x + $y;
+
+
+?>
